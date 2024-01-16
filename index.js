33a6ed79688f58c0c62a3ffce1b5f577697a1909
Oscar Donaldson <oscar@donaldson.se>
2024-01-16 15:22:43 +0100

    fix invalid input

--- index.js
+++ index.js
@@ -4,13 +4,26 @@ let interest = document.getElementById("interest");
 let paybackTime = document.getElementById("paybackTime");
 let calculateButton = document.getElementById("calculate");
 let results = document.getElementById("result");
-calculateButton.addEventListener("click", () => {
-    calculateMonthlyCost();
-});
+calculateButton.addEventListener("click", calculateMonthlyCost);
 function calculateMonthlyCost() {
-    const P = parseInt(amount.value);
-    const r = parseInt(interest.value) / 100 / 12;
-    const n = parseInt(paybackTime.value) * 12;
+    const maxAmount = 100000000;
+    const maxInterest = 5000;
+    const maxYears = 50;
+    let P = parseInt(amount.value);
+    let r = parseInt(interest.value) / 1200;
+    let n = parseInt(paybackTime.value) * 12;
+    if (P > maxAmount) {
+        alert("Ogiltig insatt summa");
+        return;
+    }
+    else if (r > maxInterest / 1200) {
+        alert("Ogiltig räntesats");
+        return;
+    }
+    else if (n > maxYears * 12) {
+        alert("Du kan inte låna för mer än 50 år.");
+        return;
+    }
     const M = Math.round((P * (r * (1 + r) ** n)) / ((1 + r) ** n - 1));
     const totalCost = M * n;
     const totalInterest = totalCost - P;
